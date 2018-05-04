29f873a93823e57300a92970c030553bf2f225ea
sommano <sommano@gmail.com>
2018-05-03 22:48:30 -0400

    tutorial

---
+++ enum.rs
@@ -0,0 +1,14 @@
+#[derive(PartialEq,PartialOrd)]
+enum Speed {
+    Slow = 10,
+    Medium, // =20
+    Fast// = 50
+}
+
+fn main() {
+    let s = Speed::Slow;
+    let Speed = s as u32;
+    println!("speed {}",speed);
+    println!("fast {}",Speed::Fast as u32);
+
+    assert!(Speed::Fast > Speed::Slow);
